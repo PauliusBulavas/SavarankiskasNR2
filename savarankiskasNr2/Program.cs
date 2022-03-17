9c3d209a11c29cfbd4ee0058a21c4d273dff4eac
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2022-03-17 20:25:49 +0200

    initial commit

---
+++ savarankiskasNr2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace savarankiskasNr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
