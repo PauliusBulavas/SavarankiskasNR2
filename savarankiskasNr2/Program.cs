2dde1dfae1af289aa1357ae7d8264e5b6f315cac
EmilijaSl <100716039+EmilijaSl@users.noreply.github.com>
2022-03-17 20:42:44 +0200

    turis

--- savarankiskasNr2/Program.cs
+++ savarankiskasNr2/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿
 
-namespace savarankiskasNr2
+using System;
+
+namespace grupine_uzduotis
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float r;
+            float pi = 3.1415926535f;
+
+            
+            r = 6371;
+
+            Console.WriteLine(4 * pi * (r * r));
+
+            Console.WriteLine(4f / 3f * pi * (r * r * r));
         }
     }
 }
