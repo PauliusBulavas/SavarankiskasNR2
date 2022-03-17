90bb283368bf22c8e5dc63f01ba82797d0032dd4
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2022-03-17 20:49:54 +0200

    izambine

--- savarankiskasNr2/Program.cs
+++ savarankiskasNr2/Program.cs
@@ -6,7 +6,44 @@ namespace savarankiskasNr2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            double izambineKvadratu = Math.Pow(10, 2) + Math.Pow(15, 2);
+            double izambine = Math.Sqrt(izambineKvadratu);
+
+            Console.WriteLine("Viso Kintamuju: ");
+            Console.WriteLine($"zemes plotas - {zemesPlotas}");
+            Console.WriteLine($"zemes turis - {zemesTuris}");
+            Console.WriteLine($"izambines ilgis - {izambine}");
+            Console.WriteLine($"suma kintamuju - {zemesPlotas + zemesTuris + izambine}");
+            Console.WriteLine($"vidurkis - {(zemesPlotas + zemesTuris + izambine) / 3}");
         }
     }
 }
